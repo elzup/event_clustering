ecf72ab04685d00766cd99c3c3ea769fb17479a8
elzzup <guild0105@gmail.com>
2015-04-24 04:53:56 +0900

    hashtag order

--- ana.py
+++ ana.py
@@ -13,6 +13,7 @@ from geoalchemy import GeometryColumn, Point
 from geoalchemy.mysql import MySQLComparator
 # utirity
 import json
+import re
 from datetime import datetime
 # config
 import config as cfg
@@ -37,20 +38,36 @@ class GeoTweets(Base):
         self.rule_id = rule_id
         self.timestamp = timestamp
 
+def get_hashtag(text):
+    pattern = r'#([\w一-龠ぁ-んァ-ヴーａ-ｚ]+)'
+    return re.findall(pattern , text)
+
 db_config = "%(engine)s://%(userid)s:%(passwd)s@%(host)s/%(name)s" % cfg.db
 engine = create_engine(db_config, encoding='utf-8')
-
 session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
-
 res = session.query(GeoTweets).filter(GeoTweets.timestamp >= '2015-04-21 10:00:00')\
-        .filter(GeoTweets.timestamp < '2015-04-21 18:00:00').limit(1000).all()
+        .filter(GeoTweets.timestamp < '2015-04-21 18:00:00').all()
+print('#- record load finish')
 
 datas = []
+tag_list = {}
 for r in res:
-    r.lat = session.scalar(r.latlng.x)
-    r.lng = session.scalar(r.latlng.y)
-    datas.append((r.lat, r.lng))
+    tags = get_hashtag(r.text)
+    for tag in tags:
+        if not tag_list.has_key(tag):
+            tag_list[tag] = []
+        tag_list[tag].append(r)
+print('#- tag grouping finish')
 
+tag_list = sorted(tag_list.items(), key=lambda x: len(x[1]), reverse=True)[:10:]
+for tag, tweets in tag_list:
+    print(tag, len(tweets))
+
+exit()
+
+#    r.lat = session.scalar(r.latlng.x)
+#    r.lng = session.scalar(r.latlng.y)
+#    datas.append((r.lat, r.lng))
 features = np.array(datas);
 
 # K-means クラスタリングをおこなう
@@ -67,7 +84,7 @@ for l, data in zip(labels, datas):
     clusters[l].append(data)
 
 for i, cluster in clusters.iteritems():
-    print('----------' + str(i) + '---------')
+    print('----------' + str(i) + '----------')
     for i in cluster:
         print(i)
 
